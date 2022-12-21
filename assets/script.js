432e84bdca2331aa0c092eff8622fe5ed2a2b41b
dspyk77 <dspyk77@gmail.com>
2022-12-20 23:14:11 -0600

    Added "Work in progress" alert

--- assets/script.js
+++ assets/script.js
@@ -240,7 +240,6 @@ function renderRooms() {
               </tr>
             </tbody>
           </table>
-    
       `
       );
     }
@@ -480,14 +479,13 @@ function emptyInputAlertDisplay() {
   emptyInputAlert.insertAdjacentHTML(
     "beforeend",
     `
-       <div id="empty-input-alert" class="alert alert-danger alert-dismissible fade show" role="alert">
+    <div id="empty-input-alert" class="alert alert-danger alert-dismissible fade show" role="alert">
       <span>
         <i class="bi bi-bandaid"></i><span class="fw-bold">Uh oh!</span>
         Look's like you left an input box empty! Make sure everthing is filled out and try again<i class="bi bi-hand-thumbs-up"></i>
       </span>
       <button class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-  
     `
   );
   emptyInputAlert.scrollIntoView();
